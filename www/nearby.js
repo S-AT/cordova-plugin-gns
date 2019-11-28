0cc73fd310474f341050523f5b7d874010aaad39
Sergey Koshelev <sergey@adagio.com>
2019-11-27 19:23:03 -0500

    .

---
+++ www/nearby.js
@@ -0,0 +1,22 @@
+var exec = require('cordova/exec');
+var nearby = {
+    subscribe: function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "NearbyPlugin", "subscribe");
+    },
+
+    unsubscribe: function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "NearbyPlugin", "unsubscribe");
+    },
+
+    publish: function (message, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "NearbyPlugin", "publish", message);
+    },
+
+    unpublish: function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "NearbyPlugin", "unpublish");
+    }
+};
+
+module.exports = nearby;
+
+
